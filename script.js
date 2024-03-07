6ef6d2ce6ab9dfad73ca95b37e3e07b888379c80
Kameron Pete <kameroncpete@gmail.com>
2024-03-07 13:32:12 +0000

    a

---
+++ script.js
@@ -0,0 +1,55 @@
+const names = ['Kameron', 'Max', 'Chelsea', 'Michelle', 'Megan', 'Lucas', 'Scott', 'Julia'];
+const occupations = ['Constractor', 'Developer', 'Translator', 'Cleaner', 'Realtor', 'Welder', 'Teacher']
+
+const freeLancers = [
+    {name:'Andre', occupation:'Dog Walker', wage:30},
+    {name:'Morgan', occupation:'Nanny', wage:50}
+]
+
+const addFreelancerIntervalId = setInterval(addFreelancer, 5000);
+
+render()
+
+function render () {
+    freeLancers.forEach((freelancer) => {
+        addFreelancerToTable(freelancer)
+    })
+    averageWage()
+}
+
+
+function addFreelancerToTable(details) { 
+    let tbl = document.querySelector('tbody');
+    let newRow = tbl.insertRow(-1);
+    let newName = newRow.insertCell(0);
+    let newOccupation = newRow.insertCell();
+    let newWage = newRow.insertCell(2);
+    newName.innerHTML = details.name;
+    newName.classList.add('name')
+    newOccupation.innerHTML = details.occupation;
+    newOccupation.classList.add("occupation")
+    newWage.innerHTML = details.wage;
+    newWage.classList.add("wage")
+}
+
+function averageWage() {
+    let total = 0
+    freeLancers.forEach((freelancer) => total = total + freelancer.wage);
+    let average = total / freeLancers.length
+    document.getElementById('averagePrice').textContent=`${average}`;
+}
+
+function addFreelancer() {
+
+    const addName = names[Math.floor(Math.random() * names.length)];
+  
+    const addOccupation = occupations[Math.floor(Math.random() * occupations.length)];
+
+    const addWage = (Math.floor(Math.random() * 10) + 1) * 10;
+  
+    freeLancers.push({name:addName, occupation:addOccupation, wage:addWage});
+    
+    render()
+  }
+//function should select the table from html
+//funtion should then call
